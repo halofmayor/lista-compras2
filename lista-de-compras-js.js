062d5087bcfc2481800ebdb702596cd979ccbdd5
halofmayor <124915837+halofmayor@users.noreply.github.com>
2023-08-12 16:04:13 +0100

    Update lista-de-compras-js.js

--- lista-de-compras-js.js
+++ lista-de-compras-js.js
@@ -1,27 +1,33 @@
 let listaCompras = [];
 let i = 1;
+let total = 0;
 
 const texto = document.querySelector('.afazer')
 const botao = document.querySelector('.butao')
 const precao = document.querySelector('.preco')
-console.log(botao)
+const remover = [];
 // Updates localStorage.
 function updateLocalStorage() {
     localStorage.setItem("data", JSON.stringify(listaCompras));
+    localStorage.setItem("total", total);
 }
 
+
 //checks localStorage if there is an item called "data" then executes the code
+const savedTotal = localStorage.getItem("total");
 const savedData = localStorage.getItem("data");
 if (savedData) {
     listaCompras = JSON.parse(savedData);
+    total = Number(savedTotal)
     render();
 }
 
 // Picks inputs value and add it to listaCompras object and save it in localStorage. (listaCompras is an array with objects inside)
 function adicionar() {
-    console.log(precao.value)
+    total += Number(precao.value);
+    console.log(total)
     const name = texto.value;
-    const preco = precao.value ? `${precao.value} €` : "" ;
+    const preco = precao.value ? precao.value : "" ;
     if (name === "" && preco === "") {
         return;
     }
@@ -39,7 +45,7 @@ function render() {
         const { name, preco } = todoObject;
         const html = `
         <div>${name}</div>
-        <div>${preco}</div>
+        <div>${preco} €</div>
         <button class="delete-todo js-delete">Delete</button>`;
         listaComprasHTML += html;
     }
@@ -47,10 +53,13 @@ function render() {
     //Updates the html
     document.querySelector('.lista-compras')
         .innerHTML = listaComprasHTML;
+    document.querySelector('.valor')
+        .innerHTML = `${total} €`
     // const deleteButton = document.querySelectorAll('.js-delete'), for each delete button we add the eventListener
     document.querySelectorAll('.js-delete')
         .forEach((deleteButton, index) => {
             deleteButton.addEventListener("click", () => {
+                total = total - listaCompras[index].preco
                 listaCompras.splice(index, 1);
                 updateLocalStorage();
                 render();
@@ -64,6 +73,7 @@ botao
         adicionar(); 
         render();
         texto.value = "";
+        precao.value = "";
     })
 
 //Enter function
@@ -73,6 +83,7 @@ texto
             adicionar();
             render();
             texto.value = "";
+            precao.value = "";
         }
     })
 
